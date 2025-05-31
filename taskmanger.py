7950be740631d523b874b385803afc201a9850e1
Michael Little <michaellittle92@gmail.com>
2025-05-31 12:51:47 +1000

    Revert "updated schema with timestamps, task_order, and projects"
    This reverts commit cd7edf24d38d3fe38b6dac16604171e32272e2de.

--- taskmanger.py
+++ taskmanger.py
@@ -11,7 +11,7 @@ class TaskManager:
         cursor = sqliteConnection.cursor()
         # SELECT * FROM Tasks
         select_all_query = '''
-        SELECT task_id, task_name, is_complete FROM Tasks
+        SELECT * FROM Tasks
         '''
         cursor.execute(select_all_query)
         # For each row, create a Task instance
@@ -26,7 +26,7 @@ class TaskManager:
         cursor = sqliteConnection.cursor()
         # SELECT * FROM Tasks
         select_all_query = '''
-        SELECT  task_id, task_name, is_complete FROM Tasks
+        SELECT * FROM Tasks
         WHERE is_complete != 1
         '''
         cursor.execute(select_all_query)
@@ -39,24 +39,11 @@ def InitializeDatabase():
     sqliteConnection = sqlite3.connect("todo.db")
     cursor = sqliteConnection.cursor()
 
-    task_table = """CREATE TABLE IF NOT EXISTS Tasks (
+    table = """CREATE TABLE IF NOT EXISTS Tasks (
     task_id INTEGER PRIMARY KEY AUTOINCREMENT,
     task_name TEXT NOT NULL,
-    task_order_index INTEGER,
-    project_id INTEGER, 
-    task_creation_timestamp TIMESTAMP,
-    task_completion_timestamp,
     is_complete INTEGER NOT NULL
     );"""
-    
-    project_table = """CREATE TABLE IF NOT EXISTS Projects(
-    project_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    project_name TEXT NOT NULL,
-    project_description TEXT
-
-    );
-    """
-    cursor.execute(project_table)
-    cursor.execute(task_table)
+    cursor.execute(table)
 
 
